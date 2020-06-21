511916654b18a6401420150fe2c75009f91256c0
initGoomba <anton.j.olsson@bredband.net>
2020-06-22 01:59:17 +0200

    Result back button

--- app/src/main/java/com/antware/thirty/ScoreActivity.java
+++ app/src/main/java/com/antware/thirty/ScoreActivity.java
@@ -3,9 +3,13 @@ package com.antware.thirty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import static com.antware.thirty.GameActivity.COMB_PER_ROUND_MESSAGE;
 import static com.antware.thirty.GameActivity.SCORE_MESSAGE;
@@ -17,13 +21,12 @@ public class ScoreActivity extends AppCompatActivity {
     private static final int COMB_VIEW_INDEX = 3;
     private static final int POINTS_ROW_START_INDEX = 2;
 
-    TextView scoreView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        scoreView = findViewById(R.id.totalScoreView);
+        TextView scoreView = findViewById(R.id.totalScoreView);
+        initBackButton();
 
         int totalScore = getIntent().getIntExtra(SCORE_MESSAGE, 0);
         int[] scorePerRound = getIntent().getIntArrayExtra(SCORE_PER_ROUND_MESSAGE);
@@ -40,6 +43,16 @@ public class ScoreActivity extends AppCompatActivity {
             comb.setText(getCombName(combPerRound[i]));
         }
 
+    }
+
+    private void initBackButton() {
+        Button backButton = findViewById(R.id.scoreBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private String getCombName(int combAsInt) {
